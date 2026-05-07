fac195b00a37bf5f7b4f432cce87593dc03e94ed
Miguel Gabriel <miguel73000515@gmail.com>
2026-05-06 22:44:22 -0300

    Adição métodos de cadastro, listagem e busca no CarroRepositorio

--- ProjetoLPOO/src/Repositorio/CarroRepositorio.java
+++ ProjetoLPOO/src/Repositorio/CarroRepositorio.java
@@ -1,4 +1,33 @@
 package Repositorio;
 
+import Modelo.Carro;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class CarroRepositorio {
+    private final List<Carro> carros = new ArrayList<>();
+
+    // Adicionar carro
+    public void adicionarcarro(Carro carro) {
+        carros.add(carro);
+    }
+
+    // Listar todos os carros
+    public List<Carro> listarCarros() {
+        return carros;
+    }
+
+    // Buscar carro pela placa
+    public Carro buscarPorPlaca(String placa) {
+        for (Carro carro : carros) {
+
+            if (carro.getPlaca().equals(placa)) {
+                return carro;
+            }
+        }
+        return null;
+    }
 }
+
