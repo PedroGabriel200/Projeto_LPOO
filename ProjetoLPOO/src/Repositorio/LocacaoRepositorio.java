87c0f093c7b2fe9346c3a384fb32a5b610c6cc40
Miguel Oliveira <miguel73000515@gmail.com>
2026-05-11 20:47:04 -0300

    Criação de ArrayList para simular banco de dados para a classe LocacaoRepositorio.

--- ProjetoLPOO/src/Repositorio/LocacaoRepositorio.java
+++ ProjetoLPOO/src/Repositorio/LocacaoRepositorio.java
@@ -1,4 +1,15 @@
 package Repositorio;
 
+import Modelo.Locacao;
+import java.util.ArrayList;
+import java.util.List;
+
 public class LocacaoRepositorio {
+
+    // Lista que simula o banco de dados em memória
+    private List<Locacao> locacoes;
+
+    public LocacaoRepositorio() {
+        this.locacoes = new ArrayList<>();
+    }
 }
