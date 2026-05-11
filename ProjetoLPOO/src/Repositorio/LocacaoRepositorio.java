5d90a4b05500e658912c89547f83b9bb54fd6e29
Miguel Oliveira <miguel73000515@gmail.com>
2026-05-11 20:57:36 -0300

    Criação de ArrayList e métodos para a classe LocacaoRepositorio.

--- ProjetoLPOO/src/Repositorio/LocacaoRepositorio.java
+++ ProjetoLPOO/src/Repositorio/LocacaoRepositorio.java
@@ -7,9 +7,26 @@ import java.util.List;
 public class LocacaoRepositorio {
 
     // Lista que simula o banco de dados em memória
-    private List<Locacao> locacoes;
+    private final List<Locacao> locacoes;
 
     public LocacaoRepositorio() {
         this.locacoes = new ArrayList<>();
     }
+
+    // Método para adicionar locação
+    public void adicionar(Locacao locacao) {
+        this.locacoes.add(locacao);
+
+    }
+
+    // Método para listar todas as locações
+    public List<Locacao> listar(){
+        return this.locacoes;
+    }
+    // Método para remover locação
+    public void remover(Locacao locacao){
+        this.locacoes.remove(locacao);
+
+    }
+
 }
