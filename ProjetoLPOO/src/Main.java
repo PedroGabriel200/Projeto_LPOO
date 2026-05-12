023d9997c595d0912d772d81a38d5afaef39870b
Miguel Oliveira <miguel73000515@gmail.com>
2026-05-11 21:08:36 -0300

    Adicionar import do LocacaoRepositorio no Main

--- ProjetoLPOO/src/Main.java
+++ ProjetoLPOO/src/Main.java
@@ -1,8 +1,13 @@
 import Interface.ClienteMenu;
+import Repositorio.LocacaoRepositorio;
 
 public class Main {
+
     public static void main(String[] args) {
+
         ClienteMenu clienteMenu = new ClienteMenu();
         clienteMenu.menu();
+
+        LocacaoRepositorio repo = new LocacaoRepositorio();
     }
 }
